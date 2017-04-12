5f16a0e7c77adc8495765eb51745c4ffe89d6342
yangf6 <yangf6@uw.edu>
2017-04-11 23:29:42 -0700

    file uploaded finished

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 "use strict";
 //create the event listener
 var varSelect = document.getElementById("varSelect");
+//var fileUploaeded;
 console.log("call")
 if(varSelect){
     varSelect.addEventListener("change", function(){
@@ -11,191 +12,66 @@ if(varSelect){
         console.log(varSelect.value)
         var varRequire = document.getElementById("variable-require-result");
         if(varSelect.value == "salinity") {
-            varRequire.innerHTML = "<button type='button' class='mdl-chip' id='csvFileUpload' accept='.csv'><span class='mdl-chip__text'>Temperature</span></button> <button type='button' class='mdl-chip'><span class='mdl-chip__text'>Density</span></button> <input type='file' name='File Upload' id='txtFileUpload' accept='.csv'/>"
+            varRequire.innerHTML = "<button type='button' class='mdl-chip' id='csvFileUpload' accept='.csv'><span class='mdl-chip__text'>Temperature</span></button> <button type='button' class='mdl-chip'><span class='mdl-chip__text'>Density</span></button> <input type='file' name='File Upload' id='txtFileUpload' onchange='startUpload(event)' accept='.csv'/> <button type='button' id='nextStep' class='mdl-chip'><span class='mdl-chip__text'>Next</span></button>"
+            //fileUploaeded = document.getElementById('txtFileUpload');
         }
         if(varSelect.value == "temperature") {
-            varRequire.innerHTML = "<button type='button' class='mdl-chip' id='csvFileUpload' accept='.csv'><span class='mdl-chip__text'>Density</span></button> <button type='button' class='mdl-chip'><span class='mdl-chip__text'>Salinity</span></button> <input type='file' name='File Upload' id='txtFileUpload' accept='.csv'/>"
+            varRequire.innerHTML = "<button type='button' class='mdl-chip' id='csvFileUpload' accept='.csv'><span class='mdl-chip__text'>Density</span></button> <button type='button' class='mdl-chip'><span class='mdl-chip__text'>Salinity</span></button> <input type='file' name='File Upload' id='txtFileUpload' onchange='startUpload(event)' accept='.csv'/> <button type='button' id='nextStep' class='mdl-chip'><span class='mdl-chip__text'>Next</span></button>"
+            //fileUploaeded = document.getElementById('txtFileUpload');
         }
         if(varSelect.value == "density") {
-            varRequire.innerHTML = "<button type='button' class='mdl-chip' id='csvFileUpload' accept='.csv'><span class='mdl-chip__text'>Temperature</span></button> <button type='button' class='mdl-chip'><span class='mdl-chip__text'>salinity</span></button> <input type='file' name='File Upload' id='txtFileUpload' accept='.csv'/>"
+            varRequire.innerHTML = "<button type='button' class='mdl-chip' id='csvFileUpload' accept='.csv'><span class='mdl-chip__text'>Temperature</span></button> <button type='button' class='mdl-chip'><span class='mdl-chip__text'>salinity</span></button> <input type='file' name='File Upload' id='txtFileUpload' onchange='startUpload(event)' accept='.csv'/> <button type='button' id='nextStep' class='mdl-chip'><span class='mdl-chip__text'>Next</span></button>"
+            //fileUploaeded = document.getElementById('txtFileUpload');
         }
     });
 }else{
     console.log("null")
 }
 
-d3.csv("/src/transactions.csv", function(error, data) {
-  	window.data = data;
-    console.log(data);
+function startUpload(evt){
+// if(fileUploaeded){
+//             fileUploaded.addEventListener('change', upload, false);
+//         }else{
+//             alert("null");
+//         }
+        // Method that checks that the browser supports the HTML5 File API
+        alert("called");
+        evt.preventDefault();
+        upload(evt);
+        function browserSupportFileUpload() {
+            var isCompatible = false;
+            if (window.File && window.FileReader && window.FileList && window.Blob) {
+                isCompatible = true;
+                alert("isCompatible");
+            }
+            return isCompatible;
+        }
 
-    // This function should help you format your data: lifted from the Plotly bubble map example:
-    function unpack(rows, key) {
-      return rows.map(function(row) { return row[key]; });
-    };
-
-    //scatter plot
-    var trace1 = {
-    	y: unpack(data, 'Penicilin'),
-    	x: unpack(data, 'Bacteria '),
-    	mode: 'markers+lines',
-    	type: 'scatter',
-    	name: 'Penicilin',
-    	text: unpack(data, 'Penicilin'),
-    	marker: { size: 15,
-    			color: 'rgb(93, 164, 214)'},
-    };
-
-    var trace2 = {
-    	y: unpack(data, 'Streptomycin '),
-    	x: unpack(data, 'Bacteria '),
-    	mode: 'markers+lines',
-    	type: 'scatter',
-    	name: 'Streptomycin',
-    	text: unpack(data, 'Streptomycin '),
-    	marker: { 
-    		size: 15,
-    		color: 'rgb(255, 65, 54)',
-    		sizeref: 2,
-            symbol: 'square'},
-    };
-
-    var trace3 = {
-    	y: unpack(data, 'Neomycin'),
-    	x: unpack(data, 'Bacteria '),
-    	mode: 'markers+lines',
-    	type: 'scatter',
-    	name: 'Neomycin',
-    	text: unpack(data, 'Neomycin'),
-    	marker: { 
-    		size: 15,
-    		color: 'rgb(44, 160, 101)',
-            sizeref: 2,
-            symbol: 'diamond'},
-    };
-
-    var dataset1 =[trace1,trace2,trace3];
-
-    var layout1 = {
-    	title:'Effecness of Different Antibotic',
-    	height: 800,
-    	width: 950
-    };
-
-    Plotly.newPlot('file-upload',dataset1,layout1);
-
-    //scatter plot about antibotic related to Gram Staining
-    var preload = {
-    	pn: unpack(data, 'Gram Staining '),
-    	bac: unpack(data, 'Bacteria '),
-    	n: unpack(data, 'Neomycin'),
-    	s: unpack(data, 'Streptomycin '),
-    	p: unpack(data, 'Penicilin')
-    };
-
-    var pos = [];
-    var neg = [];
-    for (var i=0; i<4;i++){
-    	neg[i] = [];
-    };
-    for (var i=0; i<4;i++){
-    	pos[i] = [];
-    };
-
-    for(var i=0; i<preload["pn"].length; i++){
-    	if(preload["pn"][i] == "negative"){
-            neg[0].push(preload["bac"][i]);
-            neg[1].push(preload["p"][i]);
-    		neg[2].push(preload["n"][i]);
-    		neg[3].push(preload["s"][i]);
-    	}else{
-    		pos[0].push(preload["bac"][i]);
-    		pos[1].push(preload["p"][i]);
-    		pos[2].push(preload["n"][i]);
-    		pos[3].push(preload["s"][i]);
-    	}
-    };
-
-
-    var negativeP ={
-    	x: neg[0],
-    	y: neg[1],
-    	type: 'bar',
-    	name: 'Penicilin',
-    	text: neg[1],
-    	textposition: 'bottom center'
-    };
-
-    var negativeN ={
-    	x: neg[0],
-    	y: neg[2],
-    	type: 'bar',
-    	name: 'Neomycin',
-    	text: neg[2],
-    	textposition: 'bottom center'
-    };
-
-    var negativeS ={
-    	x: neg[0],
-    	y: neg[3],
-    	type: 'bar',
-    	name: 'Streptomycin',
-    	text: neg[3],
-    	textposition: 'bottom center'
-    };
-
-    var dataset2 = [negativeP,negativeN,negativeS];
-
-    var layout2 = {
-        title:'Effecness of Different Antibotic to negative bacteria',
-        height: 800,
-        width: 950,
-    };
-
-    //document.getElementById('part2').innerHTML=neg[3];
-    Plotly.newPlot('part2',dataset2,layout2);
-
-
-
-    //penicillin related to positive and negative
-    var lpositiveP ={
-        x: pos[0],
-        y: pos[1],
-        type: 'scatter',
-        mode: 'markers',
-        name: 'positive',
-        marker: { 
-            size: 15,
-            sizeref: 2,
-            color: "purple"},
-    };
-
-    var lnegativeP ={
-    	x: neg[0],
-    	y: neg[1],
-    	type: 'scatter',
-    	mode: 'markers',
-    	name: 'negative',
-        marker: { 
-            size: 15,
-            sizeref: 2,
-            color: "pink"},	
-    };
-    var layout3 = {
-        title:'Effecness of Penicillin to Different Bacteria',
-        height: 800,
-        width: 950
-    };
-
-
-    var dataset3 =[lnegativeP,lpositiveP]
-    Plotly.newPlot('part3',dataset3,layout3);
-
-  });
-//   return {
-//     city : d.city,
-//     state : d.state,
-//     population : +d.population,
-//     land_area : +d["land area"]
-//   };
+        // Method that reads and processes the selected file
+        function upload(evt) {
+            if (!browserSupportFileUpload()) {
+                alert('The File APIs are not fully supported in this browser!');
+            } else {
+                    alert("start to read");
+                    var data = null;
+                    var file = evt.target.files[0];
+                    var reader = new FileReader();
+                    alert(file);
+                    reader.readAsText(file);
+                    reader.onload = function(event) {
+                        var csvData = event.target.result;
+                        data = $.csv.toArrays(csvData);
+                        if (data && data.length > 0) {
+                            alert('Imported -' + data.length + '- rows successfully!');
+                        
+                        }else{
+                            reader.onerror = function() {
+                                alert('Unable to read ' + file.fileName);
+                            };
+                        }
+                    }
+            }   
+        }
+}
 
 
